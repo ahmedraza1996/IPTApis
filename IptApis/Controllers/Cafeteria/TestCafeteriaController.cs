aa2ab01ae5a244e97aaca50848465ad798870cfc
Kiritoxy <adilayub2@live.com>
2020-05-23 19:22:00 +0500

    Completed API v1.0

---
+++ IptApis/Controllers/Cafeteria/TestCafeteriaController.cs
@@ -0,0 +1,37 @@
+﻿using IptApis.Shared;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IptApis.Controllers.Cafeteria
+{
+    public class TestCafeteriaController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello this is cafeteria";
+        }
+        [HttpGet]
+        public string testconnection()
+        {
+            var db = DbUtils.GetDBConnection();
+            
+            if (db.Connection.State == ConnectionState.Open)
+            {
+                return ("connection success");
+            }
+            else
+            {
+                db.Connection.Open();
+            }
+            return ("test connection");
+
+        }
+    }
+}
---
+++ IptApis/Shared/DbUtils.cs
@@ -0,0 +1,29 @@
+﻿using SqlKata.Compilers;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Shared
+{
+    public class DbUtils
+    {
+
+        public static QueryFactory GetDBConnection()
+        {
+            var connection = new SqlConnection(
+                           ConfigurationManager.AppSettings["SqlDBConn"].ToString());
+             
+
+            var compiler = new SqlServerCompiler();
+            var db = new QueryFactory(connection, compiler);
+
+            return ( db  );
+
+        }
+
+    }
+}
