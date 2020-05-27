d80eabccda52c1e52a6a8e0ee084c091b982543b
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-05-27 20:51:50 +0500

    Infix to Postfix Convertor added

---
+++ IptApis/Controllers/Search Module/QueryParser/BooleanQueryParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using IptApis.Controllers.Search_Module.TextProcessingHelpers;
+
+namespace IptApis.Controllers.Search_Module.QueryParser
+{
+    public class BooleanQueryParser
+    {
+
+
+    }
+}
---
+++ IptApis/Controllers/Search Module/QueryParser/QueryParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace IptApis.Controllers.Search_Module.QueryParser
+{
+    public class QueryParser
+    {
+        private static QueryParser Instance;
+        private Regex BooleanQueryRegex;
+        private Regex ProximityQueryRegex;
+        private Regex GeneralTextQueryRegex;
+
+        private QueryParser()
+        {
+            BooleanQueryRegex = new Regex(@"^[\w\s!&|()]+$");
+            ProximityQueryRegex = new Regex(@"^(\w+)\s(\w+)\s*/\s*(\d+)$");
+            GeneralTextQueryRegex = new Regex(@"^[\w\s]+$");
+        }
+
+        public static QueryParser GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new QueryParser();
+            }
+            return Instance;
+        }
+
+        public IList<string> Parse(string query)
+        {
+            if (ProximityQueryRegex.IsMatch(query))
+            {
+
+            }
+            else if (GeneralTextQueryRegex.IsMatch(query))
+            {
+
+            }
+            else if (BooleanQueryRegex.IsMatch(query))
+            {
+
+            }
+            else
+            {
+                throw new Exception("Invalid Query");
+            }
+            return new List<string>();
+        } 
+    }
+}
