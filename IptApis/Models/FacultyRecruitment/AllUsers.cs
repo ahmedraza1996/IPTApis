d2336e22ac73a370700021fbe8fb0f3500d1a0a6
kumarnareender <nk.malhiE@gmail.com>
2020-06-18 03:04:47 +0500

    Nareender made changes to FacultyRecruitment.Models

---
+++ IptApis/Models/FacultyRecruitment/AllUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Models.FacultyRecruitment
+{
+    public class AllUsers
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ IptApis/Models/FacultyRecruitment/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Models.FacultyRecruitment
+{
+    public class UserRole
+    {
+        public int id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Role { get; set; }
+    }
+}
