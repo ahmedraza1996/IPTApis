ecb116b1befdec22627e42065e0e23d0e3b52e3c
M.Bilal Un Nabi Khan <bilalunnabi@outlook.com>
2020-06-18 23:48:08 +0500

    apis approved for login screens

---
+++ IptApis/Models/AdminPanel/faculties.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Models.AdminPanel
+{
+    [Serializable]
+    public class faculties
+    {
+
+        public faculties(string EmpName,string Email,string MobileNumber,string EPassword)
+        {
+            this.EmpName = EmpName;
+            this.Email = Email;
+            this.MobileNumber = MobileNumber;
+            this.EPassword = EPassword;
+        }
+
+
+        //public int EmpID { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string Email { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string EPassword { get; set; }
+
+
+    }
+}
---
+++ IptApis/Models/AdminPanel/students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Models.AdminPanel
+{
+    public class students
+    {
+
+        public students(string SName, string Email, string MobileNumber,string RollNumber, string SPassword)
+        {
+            this.SName = SName;
+            this.Email = Email;
+            this.MobileNumber = MobileNumber;
+            this.RollNumber = RollNumber;
+            this.SPassword = SPassword;
+        }
+
+
+        //public int StudentID { get; set; }
+
+        public string SName { get; set; }
+
+        public string Email { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string RollNumber { get; set; }
+
+        public string SPassword { get; set; }
+
+
+
+    }
+}
