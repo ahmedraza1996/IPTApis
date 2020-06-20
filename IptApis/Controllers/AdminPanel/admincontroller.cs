76b0c7c87376bdbf9448e204230764d1ca93397d
M.Bilal Un Nabi Khan <bilalunnabi@outlook.com>
2020-06-20 17:29:24 +0500

    Updated Admin Apis

--- IptApis/Controllers/AdminPanel/admincontroller.cs
+++ IptApis/Controllers/AdminPanel/admincontroller.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 using System.Data.SqlClient;
 using System.Configuration;
 using Newtonsoft.Json;
+using System.Transactions;
 
 namespace IptApis.Controllers.AdminPanel
 {
@@ -97,6 +98,354 @@ namespace IptApis.Controllers.AdminPanel
 
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Faculty CRUD Operations work will be done here
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("admin/getFaculties")]
+        public HttpResponseMessage GetFaculties()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<IDictionary<string, object>> response;
+            response = db.Query("employee").Select("employee.EmpID", "employee.EmpName", "employee.Email", "employee.MobileNumber", "Designation.DesignationID", "Designation.DesignationTitle", "Department.DepartmentID", "Department.DepartmentName").Join("Designation", "employee.DesignationID", "Designation.DesignationID").Join("Department", "employee.DepartmentID", "Department.DepartmentID").Get().Cast<IDictionary<string, object>>();   // gets all food items from table and cast it to list of dictionary
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("admin/getFaculties/")]
+        public HttpResponseMessage GetFaculties(int id)
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<IDictionary<string, object>> response;
+            response = db.Query("employee").Where("employee.EmpID", id).Select("employee.EmpID", "employee.EmpName", "employee.Email", "employee.MobileNumber", "Designation.DesignationTitle", "Department.DepartmentName").Join("Designation", "employee.DesignationID", "Designation.DesignationID").Join("Department", "employee.DepartmentID", "Department.DepartmentID").Get().Cast<IDictionary<string, object>>();   // gets all food items from table and cast it to list of dictionary
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
+        [HttpGet]
+        public HttpResponseMessage GetDesignation()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<ADesignations> response;
+
+            response = db.Query("Designation").Get<ADesignations>();
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+
+        }
+        [HttpGet]
+        public HttpResponseMessage GetDepartment()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<ADepartments> response;
+
+            response = db.Query("Department").Get<ADepartments>();
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddFaculty(DataFaculties data)
+        {
+            string EmpName = data.EmpName;
+            string Email = data.Email;
+            string MobileNumber = data.MobileNumber;
+            string EPassword = data.EPassword;
+            int DesignationID = data.DesignationID;
+            int DepartmentID = data.DepartmentID;
+
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+
+                var query = db.Query("Employee").InsertGetId<int>(new
+                {
+                    EmpName = EmpName,
+                    Email = Email,
+                    MobileNumber = MobileNumber,
+                    EPassword = EPassword,
+                    DesignationID = DesignationID,
+                    DepartmentID = DepartmentID,
+                    RefID = 1
+                });
+
+
+                scope.Complete();  // if record is entered successfully , transaction will be committed
+
+
+                db.Connection.Close();
+                return Request.CreateResponse(HttpStatusCode.OK, query);
+            }
+        }
+
+
+
+        [HttpPost]
+        [System.Web.Http.Route("admin/DeleteFaculty")]
+        public HttpResponseMessage DeleteFaculty(DataFaculties data)
+        {
+            int id = data.EmpID;
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    db.Query("Employee").Where("EmpID", id).Delete();
+                    scope.Complete();
+                    db.Connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception ex)
+                {
+                    scope.Dispose();
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        // Students CRUD Operations work will be done here
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("admin/getStudents")]
+        public HttpResponseMessage GetStudents()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<IDictionary<string, object>> response;
+            response = db.Query("Student").Select("Student.StudentID", "Student.SName", "Student.Email", "Student.MobileNumber", "Student.RollNumber", "Student.SPassword", "Batch.BatchID", "Batch.BatchYear", "Programme.ProgrammeID", "Programme.ProgrammeName").Join("Batch", "Student.BatchID", "Batch.BatchID").Join("Programme", "Student.ProgrammeID", "Programme.ProgrammeID").Get().Cast<IDictionary<string, object>>();
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
+        [HttpGet]
+        public HttpResponseMessage GetBatch()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<ABatch> response;
+
+            response = db.Query("Batch").Get<ABatch>();
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddStudent(DataStudents data)
+        {
+            string SName = data.SName;
+            string Email = data.Email;
+            string MobileNumber = data.MobileNumber;
+            string RollNumber = data.RollNumber;
+            string SPassword = data.SPassword;
+            int BatchID = data.BatchID;
+            int ProgrammeID = data.ProgrammeID;
+
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+
+                var query = db.Query("Student").InsertGetId<int>(new
+                {
+                    SName = SName,
+                    Email = Email,
+                    MobileNumber = MobileNumber,
+                    RollNumber = RollNumber,
+                    SPassword = SPassword,
+                    BatchID = BatchID,
+                    ProgrammeID = ProgrammeID,
+                    CandidateID = 2
+                });
+
+
+                scope.Complete();  // if record is entered successfully , transaction will be committed
+
+
+                db.Connection.Close();
+                return Request.CreateResponse(HttpStatusCode.OK, query);
+            }
+        }
+
+
+
+        [HttpPost]
+        [System.Web.Http.Route("admin/DeleteStudent")]
+        public HttpResponseMessage DeleteStudent(DataStudents data)
+        {
+            int id = data.StudentID;
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    db.Query("Student").Where("StudentID", id).Delete();
+                    scope.Complete();
+                    db.Connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception ex)
+                {
+                    scope.Dispose();
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Courses CRUD Operations work will be done here
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("admin/getCourses")]
+        public HttpResponseMessage GetCourses()
+        {
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            IEnumerable<IDictionary<string, object>> response;
+            response = db.Query("Course").Select("Course.CourseID", "Course.CourseName", "Course.CourseCode").Get().Cast<IDictionary<string, object>>();
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
+
+
+        [HttpPost]
+        public HttpResponseMessage AddCourse(DataCourse data)
+        {
+            string CourseName = data.CourseName;
+            string CourseCode = data.CourseCode;
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+
+                var query = db.Query("Course").InsertGetId<int>(new
+                {
+                    CourseName = CourseName,
+                    CourseCode = CourseCode
+                });
+
+
+                scope.Complete();  // if record is entered successfully , transaction will be committed
+
+
+                db.Connection.Close();
+                return Request.CreateResponse(HttpStatusCode.OK, query);
+            }
+        }
+
+
+
+        [HttpPost]
+        [System.Web.Http.Route("admin/DeleteCourse")]
+        public HttpResponseMessage DeleteCourse(DataCourse data)
+        {
+            int id = data.CourseID;
+
+            var db = DbUtils.GetDBConnection();
+            db.Connection.Open();
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    db.Query("Course").Where("CourseID", id).Delete();
+                    scope.Complete();
+                    db.Connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception ex)
+                {
+                    scope.Dispose();
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ IptApis/Models/AdminPanel/ABatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Models.AdminPanel
+{
+    public class ABatch
+    {
+
+
+        public int BatchID { get; set; }
+
+        public string BatchName { get; set; }
+
+        public int BatchYear { get; set; }
+
+
+    }
+}
